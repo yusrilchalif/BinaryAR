685162aedcb5f51a41ec6066a454e2d1c2ad6d06
Yusril Chalif <yusrilchalif@gmail.com>
2019-07-08 19:40:01 +0700

    this is master file

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play(){
+        Application.LoadLevel("GamePlay");
+    }
+
+    public void Panduan(){
+        Application.LoadLevel("Panduan");
+    }
+
+    public void Profil(){
+        Application.LoadLevel("Profil");
+    }
+
+    public void Materi(){
+        Application.LoadLevel("Materi");
+    }
+
+    public void Quit(){
+         Application.Quit();
+         Debug.Log("quit");
+    }
+}
